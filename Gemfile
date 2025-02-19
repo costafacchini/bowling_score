c280dc4a31b962f2697cb23b881f25517f6a3074
Alan Costa Facchini <costafacchini@gmail.com>
2025-02-19 16:39:59 -0300

    add rubocop performance

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ ruby '3.3.1'
 
 gem 'rspec'
 gem 'rubocop', '~> 1.72', require: false
+gem 'rubocop-performance', require: false
 gem 'rubocop-rspec', '~> 2.3.0', require: false
 gem 'simplecov', require: false
--- lib/data_loader/validation_rule/invalid_score.rb
+++ lib/data_loader/validation_rule/invalid_score.rb
@@ -3,7 +3,7 @@ require_relative 'base'
 module ValidationRule
   class InvalidScore < Base
     def apply(input)
-      @error_message = "Invalid score : #{input}" unless (0..10).include?(input.to_i)
+      @error_message = "Invalid score : #{input}" unless (0..10).cover?(input.to_i)
       @error_message.nil?
     end
   end
